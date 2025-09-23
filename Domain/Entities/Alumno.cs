bf1a72c2554e9284fdb6f9fa72e0913968f0bbc5
FranciscoCumini <cuminifrancisco@gmail.com>
2025-09-23 18:14:25 -0300

    feat(domain): agregar entidad Alumno

---
+++ Domain/Entities/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Dni {  get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public DateOnly FechaNacimiento { get; set; }
+        public bool Activo {  get; set; }
+    }
+}
