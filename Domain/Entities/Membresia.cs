340e5b2fbed2e74c11fdb9b0a9381bb3ad8ac63a
FranciscoCumini <cuminifrancisco@gmail.com>
2025-09-23 20:36:28 -0300

    feat(infrastructure): configurar persistencia con EF Core y SQLite para Alumno, Plan, Membresia y Pago

--- Domain/Entities/Membresia.cs
+++ Domain/Entities/Membresia.cs
@@ -10,7 +10,7 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
         public int AlumnoId { get; set; }
-        public string PlanId {  get; set; }
+        public int PlanId {  get; set; }
         public DateOnly FechaInicio  { get; set; }
         public DateOnly FechaFin {  get; set; }
         public bool Activa { get; set; }
---
+++ Infrastructure/Persistence/GymDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class GymDbContext : DbContext
+    {
+        public GymDbContext(DbContextOptions<GymDbContext> options) : base(options) { }
+
+        public DbSet<Alumno> Alumnos {  get; set; }
+        public DbSet<Plan> Planes { get; set; }
+        public DbSet<Membresia> Membresias { get; set; }
+        public DbSet<Pago> Pagos { get; set; }
+
+    }
+}
