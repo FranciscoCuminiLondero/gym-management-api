0f1ac30e3aeaaa51458fdfb661e39efdd70fd005
FranciscoCumini <cuminifrancisco@gmail.com>
2025-09-25 17:30:27 -0300

    feat(infrastructure): implementar BaseRepository y AlumnoRepository heredando de él

---
+++ Infrastructure/Persistence/Repositories/AlumnoRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Application.Abstractions;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class AlumnoRepository : BaseRepository<Alumno>, IAlumnoRepository
+    {
+        private readonly GymDbContext _context;
+        public AlumnoRepository(GymDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public bool ExistsByDni(string dni)
+        {
+            return _context.Alumnos.Any(a => a.Dni == dni);
+        }
+    }
+}
