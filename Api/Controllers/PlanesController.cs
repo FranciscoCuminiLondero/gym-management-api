04c2e6e74c40d9dac9bd3625bfaa518445d419da
FranciscoCumini <cuminifrancisco@gmail.com>
2025-10-02 16:40:36 -0300

    feat(presentation): agregar PlanesController

---
+++ Api/Controllers/PlanesController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Services;
+using Contract.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlanesController : ControllerBase
+    {
+        private readonly IPlanService _planService;
+        public PlanesController(IPlanService planService)
+        {
+            _planService = planService; 
+        }
+
+        [HttpGet]
+        public ActionResult<List<PlanResponse>> GetPlanesActivos()
+        {
+            var planes = _planService.GetPlanesActivos();
+            return Ok(planes);
+        }
+    }
+}
