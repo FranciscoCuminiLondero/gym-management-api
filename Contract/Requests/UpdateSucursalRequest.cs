a9557a9e36b47fb48243b988e5f86e063000071f
FranciscoCumini <cuminifrancisco@gmail.com>
2025-10-24 19:36:53 -0300

    feat: agregar DTOs para actualización de sucursales y salas

---
+++ Contract/Requests/UpdateSucursalRequest.cs
@@ -0,0 +1,10 @@
+namespace Contract.Requests
+{
+    public class UpdateSucursalRequest
+    {
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+        public string? Telefono { get; set; }
+        public string? Email { get; set; }
+    }
+}
